dc2108868ab50a22e075366e7179820957e4c930
St. Pardon <okc4pardon@gmail.com>
2025-09-04 20:33:00 +0100

    :white_check_mark: login and logout endpoint

--- app/extensions.py
+++ app/extensions.py
@@ -5,3 +5,4 @@ from flask_marshmallow import Marshmallow
 db = SQLAlchemy()
 migrate = Migrate()
 ma = Marshmallow()
+BLOCKLIST = set()
--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, get_jwt, jwt_required
 from app.models.user_model import User
-from app.extensions import db
+from app.extensions import db, BLOCKLIST
 from app.schema.user_schema import User_schema
 
 user_schema = User_schema(session=db.session)
@@ -33,3 +33,28 @@ def register():
                         "token":access_token,
                         "user": user_schema.dump(user)
                     }}), 201
+
+@auth_bp.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(email=data.get("email")).first()
+
+    if not user or not user.check_password(data.get("password")):
+        return jsonify({"status": 401,
+                        "message": "Invalid credentials"}), 401
+
+    access_token = create_access_token(identity=str(user.id))
+
+    return jsonify({"status": 200,
+                    "message": "Login successful",
+                    "data": {
+                        "token":access_token,
+                        "user": user_schema.dump(user)
+                    }})
+
+@auth_bp.route("/logout", methods=["POST"])
+@jwt_required()
+def logout():
+    jti = get_jwt()["jti"]
+    BLOCKLIST.add(jti)
+    return jsonify({"status": 200, "message": "Successfully logged out"}), 200
